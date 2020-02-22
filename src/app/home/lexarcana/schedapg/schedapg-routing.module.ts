c3e46e5635f63ae68ae7868e0e5f255bdf371037
Mario Mariottide <giordano.forconi@gmail.com>
2020-02-22 17:09:27 +0100

    Aggiornamento grafica Lex Arcana e rimozione pagine eccesso

--- src/app/home/lexarcana/schedapg/schedapg-routing.module.ts
+++ src/app/home/lexarcana/schedapg/schedapg-routing.module.ts
@@ -8,19 +8,7 @@ const routes: Routes = [
     path: '',
     component: SchedapgPage,
     children: [
-      { path: '', redirectTo: 'tabstat', pathMatch: 'full' },
-      {
-         path: 'tabstat',
-         loadChildren: () => import('./tabstat/tabstat.module').then( m => m.TabstatPageModule)
-      },
-      {
-        path: 'tabskill',
-        loadChildren: () => import('./tabskill/tabskill.module').then( m => m.TabskillPageModule)
-      },
-      {
-        path: 'tabequip',
-        loadChildren: () => import('./tabequip/tabequip.module').then( m => m.TabequipPageModule)
-      }
+      { path: '', redirectTo: 'schedapersonaggio', pathMatch: 'full' },
     ]
   }];
 
